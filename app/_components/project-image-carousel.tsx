cf5cc320abece2d8eaf82d821155047e417ccc32
matheusfriguette <matheus.f.riguette@gmail.com>
2024-10-10 13:54:16 -0300

    fix: ring button carousel

--- app/_components/project-image-carousel.tsx
+++ app/_components/project-image-carousel.tsx
@@ -14,6 +14,10 @@ const theme: CustomFlowbiteTheme = {
       base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth",
       snap: "snap-x",
     },
+    control: {
+      base: "inline-flex h-8 w-8 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 sm:h-10 sm:w-10",
+      icon: "h-5 w-5 text-white dark:text-gray-800 sm:h-6 sm:w-6",
+    },
   },
 };
 
